d051c63adc83e74bd304a67d8bb2746ceda4454b
BharathiSure <39580182+BharathiSure@users.noreply.github.com>
2025-02-09 05:27:43 +0000

    addedOwnLogic

--- src/01/Challenge/ch1_palindrome.cpp
+++ src/01/Challenge/ch1_palindrome.cpp
@@ -6,17 +6,19 @@
 
 #include <iostream>
 #include <algorithm>
-
+#include<string>
+#include<cctype>
 // is_palindrome()
 // Summary: This function receives a string and returns true if the string is a palindrome, false otherwise.
 // Arguments:
 //           str: The string to analyze.
 // Returns: A boolean value. True for palindromes, false otherwise.
 bool is_palindrome(std::string str){
+    std::transform(str.begin(),str.end(),str.begin(),::tolower);
+    std::string rev=str;
+    std::reverse(rev.begin(),rev.end());
+    return rev == str;
 
-    // Write your code here
-
-    return false;
 }
 
 // Main function
